7571a01b9c3b7a1045ca6be5bb987254cf43d021
DavidKocman36 <80544933+DavidKocman36@users.noreply.github.com>
2022-07-12 22:28:37 +0200

    Delete Second year/3. semestr directory

--- Second year/3. semestr/4. semestr/ITW/ITW2/README.txt
+++
@@ -1,5 +0,0 @@
-Odkaz na web:
-https://www.stud.fit.vutbr.cz/~xkocma08/
-Nachází se také v logu stránky v navigaci.
-
-Odevzdaná verze neobsahuje obrázky.
--- Second year/3. semestr/IFJ/numbers.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include <stdlib.h>
-
-int main(int argc, char**argv){
-    float f = atof(argv[1]);
-    printf("Number %s = %a\n", argv[1], f);
-}
